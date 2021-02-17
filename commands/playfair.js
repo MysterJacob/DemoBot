e980bf83f6c6364e708f9eac59c871802f008faa
MysterJacob <kubapoloszyk@gmail.com>
2021-02-17 15:04:29 +0100

    playfair

---
+++ commands/playfair.js
@@ -0,0 +1,7 @@
+module.exports.run = function(msg,bot,args){
+    msg.reply("eb ea fd f9 c4 88 d7 8c e0 8a d2 8c d3 d3 e0 8f d9 e0 cd 8c c9 8c d1 89 8c c2");
+}
+module.exports.help = {
+    "names":["roxy","rx"],
+    "description":"Pomg."
+}
