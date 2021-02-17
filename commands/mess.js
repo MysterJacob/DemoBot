1ed813494c659f53e17cde538da33627962633cf
MysterJacob <kubapoloszyk@gmail.com>
2021-02-17 15:54:19 +0100

    messchallenge

---
+++ commands/mess.js
@@ -0,0 +1,12 @@
+/*
+Hahaha , I hacked into the program. They will never know how to decode this!
+Uppss, how can I remove it?!
+aW1wb3J0IGN2MgppbXBvcnQgbnVtcHkgYXMgbnAKaW1nID0gY3YyLmltcmVhZCgnU0VDUkVULnBuZycsMCkKCnN5LHN4ID0gaW1nLnNoYXBlCnByaW50KHN4LHN5KQpicm9rZW4gPSBucC5vbmVzKCgxLHN4KnN5LDEpLG5wLnVpbnQ4KSoyNTUKCgpmb3IgaSBpbiByYW5nZSgxLHN4KnN5KToKICAgIAogICAgeCA9IGkgJSAyODAKICAgIHkgPSBpIC8vIDI4MAogICAgcGl4ZWwgPSBpbWdbeS0xLHgtMV0KCiAgICBicm9rZW5bMCxpXSA9IHBpeGVsCgogICAgICAgIApjdjIuaW13cml0ZSgiMTAweDI4MG1lc3NlZC5wbmciLGJyb2tlbikK
+*/
+module.exports.run = function(msg,bot,args){
+    msg.reply("Can you make sens of it? My programs's ``SOURCE CODE`` messed up and that was the output.",{ files: ["./100x280messed.png.zip"] });
+}
+module.exports.help = {
+    "names":["mess"],
+    "description":"Mess challenge."
+}
